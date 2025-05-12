f1656e3b017ee00e7bd12ada0e34a3192437a685
Jacobo Zuluaga <jacobozuluaga80@gmail.com>
2025-05-12 00:07:35 -0500

    refactor: remove initial sample data insertion from catalog service

--- microservices/catalog_service/app.py
+++ microservices/catalog_service/app.py
@@ -23,31 +23,6 @@ def init_db(app):
     with app.app_context():
         # Create all tables
         db.create_all()
-        
-        # Check if we need to add some initial data
-        from models.book import Book
-        if Book.query.count() == 0:
-            # Add some sample books
-            sample_books = [
-                Book(
-                    title="The Great Gatsby",
-                    author="F. Scott Fitzgerald",
-                    description="A story of the fabulously wealthy Jay Gatsby",
-                    price=19.99,
-                    stock=10,
-                    seller_id=1
-                ),
-                Book(
-                    title="To Kill a Mockingbird",
-                    author="Harper Lee",
-                    description="The story of racial injustice and the loss of innocence",
-                    price=15.99,
-                    stock=8,
-                    seller_id=1
-                )
-            ]
-            db.session.bulk_save_objects(sample_books)
-            db.session.commit()
 
 if __name__ == '__main__':
     app = create_app()
